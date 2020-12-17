2ea36e56e7a2204ed2f7a7c54059966a4e386835
‘Juliano <julianoperins@gmail.com>
2020-12-17 15:30:49 -0500

    Successfully called two apis

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
-// eaa4a6a512eeccb553a8182644a8d19c
+const geocode = require("./utils/geocode");
 
-// http://api.weatherstack.com/current?access_key=eaa4a6a512eeccb553a8182644a8d19c&query=Jersey%20City
+const forecast = require("./utils/forecast");
 
-// http://api.weatherstack.com/current?access_key=eaa4a6a512eeccb553a8182644a8d19c&query=37.8267,-122.4233
-
-const request = require("postman-request");
-
-const url =
-  "http://api.weatherstack.com/current?access_key=eaa4a6a512eeccb553a8182644a8d19c&query=37.8267,-122.4233";
-
-// request('http://www.google.com', function (error, response, body) {
-//   console.log('error:', error); // Print the error if one occurred
-//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-//   console.log('body:', body); // Print the HTML for the Google homepage.
-// });
-
-request(url, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.current);
+geocode("New York", (error, data) => {
+  console.log("Error", error);
+  console.log("Data", data);
+  forecast(data.latitude, data.longitude, (error, data) => {
+    console.log("Error", error);
+    console.log("Data", data);
+  });
 });
